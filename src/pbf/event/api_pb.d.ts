206c99815259a3df458755ec3611b7e0700c7ee8
NaoNaoOnline <>
2023-07-27 15:44:15 +0000

    update generated code

--- src/pbf/event/api_pb.d.ts
+++
@@ -1,6 +0,0 @@
-import * as jspb from "google-protobuf";
-
-import * as pbf_event_create_pb from "../../pbf/event/create_pb";
-import * as pbf_event_delete_pb from "../../pbf/event/delete_pb";
-import * as pbf_event_search_pb from "../../pbf/event/search_pb";
-import * as pbf_event_update_pb from "../../pbf/event/update_pb";
--- src/pbf/host/api_pb.d.ts
+++
@@ -1,6 +0,0 @@
-import * as jspb from "google-protobuf";
-
-import * as pbf_host_create_pb from "../../pbf/host/create_pb";
-import * as pbf_host_delete_pb from "../../pbf/host/delete_pb";
-import * as pbf_host_search_pb from "../../pbf/host/search_pb";
-import * as pbf_host_update_pb from "../../pbf/host/update_pb";
